7da39132d0e541725c2195a0984128e80807ff6e
Nilesh <nileshparmar2005@gmail.com>
2025-07-19 18:05:08 +0530

    feat: Add Vercel Analytics for visitor tracking

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import Weather from './components/Weather';
+import { Analytics } from '@vercel/analytics/react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Weather />
+      <Analytics />
     </div>
   );
 }
